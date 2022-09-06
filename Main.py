9cfb25d4f908ae3976d1f7e9855aa3b9b471d073
Steive-URK21CS1188 <110010600+Steive-URK21CS1188@users.noreply.github.com>
2022-09-06 12:02:23 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,13 +11,42 @@ class Queue:
 
   def enqueue(self, data) -> None:
     # Write your code here
-
+    if self.last==None:
+      self.last=Node(data)
+      self.last.next=None
+      self.last.data=data
+      self.head=self.last
+    else:
+      t=Node(data)
+      self.last.next=t
+      t.data=data
+      t.next=None
+      self.last=t
   def dequeue(self) -> None:
     # Write your code here
-
+    t=self.head
+    if self.front==None:
+      return None
+    else:
+      if t.next!=None:
+        t=t.next
+        return self.head.data
+        self.head=t
+      else:
+        return self.head.data
+        self.head=None
+        self.last=None
   def status(self) -> None:
     # Write your code here
-
+    t=self.head
+    if self.front==None and self.last==None:
+      print("None")
+    while(t!=None):
+      print(t.data,end="")
+      print("=>",end="")
+      t=t.next
+    if t==None:
+      print("None")
 
 # Do not change the following code
 queue = Queue()
